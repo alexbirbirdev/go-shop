53fe21df330c14315d996cf7cb07cd9600fbba7e
Alex <alexbirdev@gmail.com>
2025-05-20 22:37:08 +0300

    Изменена модель категорий для вложенностей

--- internal/models/category.go
+++ internal/models/category.go
@@ -4,5 +4,7 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Name string `json:"name" gorm:"not null"`
+	Name     string     `json:"name" gorm:"not null"`
+	ParentID *uint      `json:"parent_id" gorm:"default:NULL"`
+	Children []Category `json:"children" gorm:"foreignKey:ParentID"`
 }
