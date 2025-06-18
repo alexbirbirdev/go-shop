d983075f3fb968a76aadf1054b8c797e22ae4395
Alex <alexbirdev@gmail.com>
2025-06-18 16:33:06 +0300

    try5

--- backend/config/getEnv.go
+++ backend/config/getEnv.go
@@ -1,6 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 func GetEnv() {
+	fmt.Printf("DB_HOST: %s\n", os.Getenv("DB_HOST"))
 	// err := godotenv.Load()
 	// if err != nil {
 	// 	log.Println("Error loading .env file")
